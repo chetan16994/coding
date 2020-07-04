606a294c96b59dfe1af0749f0a5cb6cbfe033906
chetan16994 <54647452+chetan16994@users.noreply.github.com>
2020-07-03 18:25:08 -0600

    Update TwoPointer.java

--- TwoPointer.java
+++ TwoPointer.java
@@ -2,6 +2,9 @@ import java.util.Arrays;
 
 public class TwoPointer {
 
+    // https://www.geeksforgeeks.org/given-an-array-a-and-a-number-x-check-for-pair-in-a-with-sum-as-x/
+    // check using hashing
+    
     static boolean isPairSum(int[] arr,int n){
         Arrays.sort(arr);
         int i=0;
